2d4b5393d0e7bec24ca1fc9b319c5519693000fa
Sergio <sergioruza22@gmail.com>
2023-09-04 19:40:24 -0300

    feat: Números Ímpares - 1067

---
+++ 1067.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const numero = Number(lines[0]);
+
+for (let i = 1; i <= numero; i += 1) {
+  if (i % 2 !== 0) console.log(i)
+}

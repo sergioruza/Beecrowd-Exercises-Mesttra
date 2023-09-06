218db4c143ba5ddac8eca594293bec8cb6a45d2e
Sergio <sergioruza22@gmail.com>
2023-09-06 19:45:18 -0300

    feat: Sequencia IJ 3 - 1097

---
+++ 1097.js
@@ -0,0 +1,17 @@
+let I = 1;
+let J = 7;
+
+let count = 1;
+
+while (I <= 9) {
+  console.log(`I=${I} J=${J}`);
+
+  if (count === 3) {
+    count = 1;
+    I += 2;
+    J += 4;
+  } else {
+    count += 1
+    J -= 1
+  }
+}

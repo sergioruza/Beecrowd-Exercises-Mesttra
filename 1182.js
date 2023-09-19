0ff6ee8aaeef97779592746179d1d395e2fc5559
Sergio <sergioruza22@gmail.com>
2023-09-19 16:45:22 -0300

    feat: Coluna na Matriz - 1182

---
+++ 1182.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const colunaOperacao = Number(lines.shift());
+const operacao = lines.shift();
+
+function matriz() {
+  const numeros = lines.map((e) => Number(e));
+
+  const matriz = [];
+
+  for (let i = 0; i < 12; i += 1) {
+    const linha = [];
+
+    for (let j = 0; j < 12; j += 1) {
+      linha.push(numeros.shift());
+    }
+
+    matriz.push(linha);
+  }
+
+  return matriz;
+}
+
+function operacaoColunaMatriz() {
+  const matrizPronta = matriz();
+
+  const colunaMatriz = matrizPronta.map((e) => e[colunaOperacao]);
+
+  const soma = colunaMatriz.reduce((acc, cur) => acc + cur);
+
+  if (operacao === 'S') {
+    return soma.toFixed(1);
+  } else {
+    return (soma / colunaMatriz.length).toFixed(1);
+  }
+}
+
+console.log(operacaoColunaMatriz());

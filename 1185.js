8a6ce839b6c4b7c24822fa5487071a8c2cfcde8a
Sergio <sergioruza22@gmail.com>
2023-09-19 21:41:43 -0300

    error: 1185 - Acima da Diagonal Secundária

---
+++ 1185.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const operacao = lines.shift();
+
+function matriz() {
+  const numeros = lines.map((e) => Number(e));
+
+  const matriz = [];
+
+  for (let i = 0; i < 12; i += 1) {
+    const linha = [];
+
+    for (let j = 0; j < 12; j += 1) {
+      linha.push(numeros.shift());
+    }
+
+    matriz.push(linha);
+  }
+
+  return matriz;
+}
+
+function operacaoMatriz() {
+  const matrizPronta = matriz();
+
+  let soma = 0;
+  let contador = 0;
+
+  matrizPronta.forEach((e, i) => {
+    for (let j = 0; j < 12; j += 1) {
+      if (j !== 11 && j !== (11 - i)) {
+        soma += e[j];
+        contador += 1;
+      }
+    }
+  });
+
+  if (operacao === 'S') {
+    return soma.toFixed(1);
+  } else {
+    return (soma / contador).toFixed(1);
+  }
+}
+
+console.log(operacaoMatriz());

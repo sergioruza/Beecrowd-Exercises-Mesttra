f612b2e0a092d26d0acfeb29d958efc188396aac
Sergio <sergioruza22@gmail.com>
2023-09-27 16:28:41 -0300

    feat: Vitória e a Indecisão - 1924

---
+++ 1924.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log('Ciencia da Computacao');

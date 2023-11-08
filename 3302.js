dbd1c128bdb0439a0941926f6ef58dadd49eaf18
Sergio <sergioruza22@gmail.com>
2023-11-08 16:04:14 -0300

    feat: Resposta Certa - 3302

---
+++ 3302.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+var input = require('fs').readFileSync(currentFile + '/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+const cases = Number(lines.shift())
+
+for (let i = 0; i < cases; i += 1) {
+  console.log(`resposta ${i+1}: ${lines[i]}`);
+}
